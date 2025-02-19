5d83adac34cfceb9675552b47b2406672db895af
Kroaxys <mellar08sw@gmail.com>
2025-02-19 11:34:15 +0100

    Declutered MainPage.xaml.cs and made better comments.

--- Calculator/MainPage.xaml.cs
+++ Calculator/MainPage.xaml.cs
@@ -6,11 +6,11 @@ namespace Calculator
 {
     public partial class MainPage : ContentPage
     {
-        double temp = 0; //Temporary variable to store the result of the calculation
-        string symbol = ""; //Variable to store the symbol of the operation
-        double operand = 0; //Variable to store the number entered by the user
-        int numberbuttonpresses = 0; //Variable to store the number of times a number button has been pressed
-        int symbolbuttonpresses = 0; //Variable to store the number of times a symbol button has been pressed
+        double temp = 0; 
+        string symbol = ""; 
+        double operand = 0; 
+        int numberbuttonpresses = 0; 
+        int symbolbuttonpresses = 0; 
         bool calculationdone = false; 
 
 
@@ -21,31 +21,32 @@ namespace Calculator
        
         private void CommaButton(object sender, EventArgs e)
         {
-            Button button = (Button)sender; //Get the button that was clicked
-            EntryCalculation.Text += button.Text; //Display the comma in the calculation field
-            operand = Convert.ToDouble(button.Text); //Get the number from the result field and add the comma
+            Button button = (Button)sender;
+            EntryCalculation.Text += button.Text; 
+            operand = Convert.ToDouble(button.Text); 
 
 
         }
 
         private void NumberButton(object sender, EventArgs e) 
         {
-            Button button = (Button)sender; //Get the button that was clicked
+            Button button = (Button)sender;
 
-            operand = (operand * 10) + Convert.ToDouble(button.Text); //Get the number from the button and add it to the operand
+            operand = (operand * 10) + Convert.ToDouble(button.Text); 
 
-            Debug.WriteLine($"{operand}operand {symbol}symbol {temp}temp number button inserted1");
             
-            EntryCalculation.Text += button.Text; //Display the number in the calculation field along with any previously inputed numbers
-            numberbuttonpresses++; //Increment the number of times a number button has been pressed by one
-            symbolbuttonpresses = 0; //Reset the number of times a symbol button has been pressed
-            Debug.WriteLine($"{operand}operand {symbol}symbol {temp}temp number button inserted2");
+            
+            EntryCalculation.Text += button.Text;
+            numberbuttonpresses++;
+            symbolbuttonpresses = 0; 
+            
         } 
 
         private void SymbolButton(object sender, EventArgs e)
         {
 
-            if (symbol != "" && symbolbuttonpresses == 0 && numberbuttonpresses == 1) //If there is already a symbol, calculate the result. Check if the user has entered a number before pressing a symbol button
+            if (symbol != "" && symbolbuttonpresses == 0 && numberbuttonpresses == 1)  //This whole if and else if checks wheter a symbol has been used 
+            //previously and wheter we should put operand in temp of if we shall ignore it. Helps with using symbols right after having done a calculation.
             {
                 Calculate();
             }
@@ -60,7 +61,7 @@ namespace Calculator
             }
             else if (calculationdone == false)
             {
-                temp = operand; //Set the temp variable to the operand
+                temp = operand; 
             }
             else if (calculationdone == true) 
             {}
@@ -69,27 +70,27 @@ namespace Calculator
                 EntryResult.Text = "Something has gone wrong";
             }
 
-
-                Debug.WriteLine($"{operand}operand {symbol}symbol {temp}temp symbol button inserted1");
-
-            operand = 0; //Reset operand so that the user can enter a new number
-            Button button = (Button)sender; //Get the button that was clicked
-            symbol = button.Text; //Set the symbol to the symbol variable of the button that was clicked
-            EntryCalculation.Text += $" {symbol} "; //Display the symbol in the calculation field
-            symbolbuttonpresses++; //Increment the number of times a symbol button has been pressed by one
-            numberbuttonpresses = 0; //Reset the number of times a number button has been pressed
-            Debug.WriteLine($"{operand}operand {symbol}symbol {temp}temp symbol button inserted2");
+            operand = 0; 
+            
+            Button button = (Button)sender; 
+            symbol = button.Text; 
+            
+            EntryCalculation.Text += $" {symbol} "; 
+            
+            symbolbuttonpresses++; 
+            numberbuttonpresses = 0;
+            
 
         }
 
         private void AnswerButton(object sender, EventArgs e)
         {
-            if (symbolbuttonpresses == 0 && numberbuttonpresses > 0) //Check if the user has entered a number before pressing the answer button
+            if (symbolbuttonpresses == 0 && numberbuttonpresses > 0) 
             {
                 Calculate();
-                EntryResult.Text = temp.ToString(); //Display the result in the result field
-                EntryCalculation.Text = temp.ToString(); //Display the result in the calculation field
-                operand = 0; //reset operand
+                EntryResult.Text = temp.ToString(); 
+                EntryCalculation.Text = temp.ToString(); 
+                operand = 0; 
 
             }
             else
@@ -106,19 +107,19 @@ namespace Calculator
 
             if (operand == 0)
             {
-                Debug.WriteLine($"{operand}operand {symbol}symbol {temp}temp if opperand == 0 begining");
+                
                 switch (symbol) //Perform the calculation based on the symbol
                 {
-                    case "+": //Addition
+                    case "+": 
                         temp += temp;
                         break;
-                    case "-": //Subtraction
+                    case "-": 
                         temp -= temp;
                         break;
-                    case "*": //Multiplication 
+                    case "*": 
                         temp *= temp;
                         break;
-                    case "/": //Division
+                    case "/": 
                         if (temp == 0) //Check if the user is trying to divide by 0
                         {
                             DisplayAlert("Fel", "Division med 0 är inte tillåtet", "Ok");
@@ -131,24 +132,24 @@ namespace Calculator
 
 
                 }
-                Debug.WriteLine($"{operand}operand {symbol}symbol {temp}temp if opperand == 0 end");
+                
             }
             else 
             {
-                Debug.WriteLine($"{operand}operand {symbol}symbol {temp}temp else begining");
-                switch (symbol) //Perform the calculation based on the symbol
+                
+                switch (symbol) 
                 {
-                    case "+": //Addition
+                    case "+": 
                         temp += operand;
                         break;
-                    case "-": //Subtraction
+                    case "-": 
                         temp -= operand;
                         break;
-                    case "*": //Multiplication
+                    case "*": 
                         temp *= operand;
                         break;
-                    case "/": //Division
-                        if (operand == 0) //Check if the user is trying to divide by 0
+                    case "/": 
+                        if (operand == 0) 
                         {
                             DisplayAlert("Fel", "Division med 0 är inte tillåtet", "Ok");
                             Clear();
@@ -157,12 +158,12 @@ namespace Calculator
                         temp /= operand;
                         break;
                         }
-                Debug.WriteLine($"{operand}operand {symbol}symbol {temp}temp else ending");
+               
             }
             
 
-            operand = 0; //Reset operand
-            symbol = ""; //Reset symbol
+            operand = 0; 
+            symbol = ""; 
             calculationdone = true;
 
         }
@@ -176,8 +177,9 @@ namespace Calculator
             symbol = "";
             symbolbuttonpresses = 1;
             numberbuttonpresses = 0;
+            calculationdone = false;
         }
-        private void ClearButton(object sender, EventArgs e) //Clear all fields if the clear button is clicked
+        private void ClearButton(object sender, EventArgs e) 
         {
             Clear();
         }
